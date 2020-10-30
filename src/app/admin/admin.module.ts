8f8756219d7515368ab1d8a948b5055b425c786f
Adis Civgin <civgin.adis@gmail.com>
2020-10-30 22:36:47 +0100

    first commit

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { ManageProjectsComponent } from './manage-projects.component';
+import { AddProjectDialogComponent } from './add-project-dialog.component';
+import { DeleteDialogComponent } from './delete-dialog.component';
+import { ManagePermissionsComponent } from './manage-permissions.component';
+import { AdminRoutingModule } from './admin-routing.module';
+import { AddProjectUserDialogComponent } from './add-project-user-dialog.component';
+import { CoreModule } from '../core/core.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    AdminRoutingModule,
+    CoreModule
+  ],
+  exports: [],
+  declarations: [
+    ManageProjectsComponent,
+    ManagePermissionsComponent,
+    AddProjectDialogComponent,
+    DeleteDialogComponent,
+    AddProjectUserDialogComponent
+  ],
+  providers: [],
+  entryComponents: [
+    AddProjectDialogComponent,
+    DeleteDialogComponent,
+    AddProjectUserDialogComponent
+  ]
+})
+export class AdminModule {}
